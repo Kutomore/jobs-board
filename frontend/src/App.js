03c4398b8199de9723da21255f7684d8a7f33354
Kutomore <kutomore@gmail.com>
2021-05-03 18:14:15 -0300

    Finishing up See More

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button, CardFooter,
-  Input, InputGroup, Label, FormGroup, Form
+  Input, Label, FormGroup, Form,
+  Modal, ModalBody, ModalFooter, ModalHeader
 } from "reactstrap";
 
 class App extends Component {
@@ -12,7 +13,19 @@ class App extends Component {
     filters: {
       location: '',
       description: ''
-    }
+    },
+    seeMoreModalData: {
+      id: '',
+      title: '',
+      url: '',
+      company: '',
+      description: '',
+      how_to_apply: '',
+      company_logo: '',
+      type: '',
+      created_at: ''
+    },
+    seeMoreModal: false
   }
 
   fetchJobs(){
@@ -22,6 +35,18 @@ class App extends Component {
         })
       }
     )
+  }
+
+  toggleSeeMoreModal(){
+    this.setState({
+      seeMoreModal: ! this.state.seeMoreModal
+    });
+  }
+
+  seeMoreModal(id, title, url, company, description, how_to_apply, company_logo, type, created_at) {
+    this.setState({
+      seeMoreModalData: { id, title, url, company, description, how_to_apply, company_logo, type, created_at }, seeMoreModal: ! this.state.seeMoreModal
+    });
   }
 
   componentDidMount(){
@@ -50,7 +75,7 @@ class App extends Component {
             <CardText dangerouslySetInnerHTML={{__html: job.description}}/>
           </CardBody>
           <CardFooter>
-            <Button>See more</Button>
+            <Button onClick={this.seeMoreModal.bind(this, job.id, job.title, job.url, job.company, job.description, job.how_to_apply, job.company_logo, job.created_at, job.type)}>See more</Button>
           </CardFooter>
         </Card>
       </div>
@@ -80,6 +105,27 @@ class App extends Component {
 
     return (
       <div className="App container">
+        <Modal isOpen={this.state.seeMoreModal} toggle={this.toggleSeeMoreModal.bind(this)}>
+          <ModalHeader toggle={this.toggleSeeMoreModal.bind(this)}>
+            {this.state.seeMoreModalData.title}
+            <div>
+              <h6 className="mb-2 text-muted card-subtitle">{this.state.seeMoreModalData.type}</h6>
+              <h6 className="mb-2 text-muted card-subtitle">Posted {this.state.seeMoreModalData.created_at}</h6>
+            </div>
+          </ModalHeader>
+          <ModalBody>
+            <div className="crop-image">
+              <CardImg top src={this.state.seeMoreModalData.company_logo} alt={this.state.seeMoreModalData.company}/>
+            </div>
+            <h6>You'd be working at <b>{this.state.seeMoreModalData.company}</b></h6>
+            <div className="modal-text" dangerouslySetInnerHTML={{__html: this.state.seeMoreModalData.description}}/>
+          </ModalBody>
+          <ModalFooter className="row" style={{borderTop: 0}}>
+            <h6 className="col-md-12">How to apply: <div dangerouslySetInnerHTML={{__html: this.state.seeMoreModalData.how_to_apply}}/>{' '}</h6>
+            <Button color="secondary" onClick={this.toggleSeeMoreModal.bind(this)}>Close</Button>
+          </ModalFooter>
+        </Modal>
+
         <div className={'row'}>
           <div className={"col-md-9"}>
             {jobs}
